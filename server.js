dc391d00d53513c08f96d032c1d8c38b3abf436a
Natividad Alvarez <tivialvarez@gmail.com>
2020-10-14 14:59:08 -0400

    Set public folder and view engine

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const server = express();
+const path = require("path");
+
+server.use(express.static(path.join(__dirname, "public")));
+server.set("view engine", "ejs");
 
 server.use(express.urlencoded({extended: true}));
 server.use(express.json());
+
 
 const routes = require("./routes");
 server.use("/", routes)
