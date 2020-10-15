d130ce9f58a54e1d12baa4ba392ce74371081369
Natividad Alvarez <tivialvarez@gmail.com>
2020-10-15 13:06:33 -0400

    Made seed and migration for views table

---
+++ models/migrations/20201015130021_create_views_table.js
@@ -0,0 +1,11 @@
+exports.up = function(knex) {
+    return knex.schema.createTable("View", table=>{
+        table.integer("user_id").notNullable().references("id").inTable("User").onDelete("CASCADE").onUpdate("CASCADE");
+        table.integer("post_id").notNullable().references("id").inTable("Post").onDelete("CASCADE").onUpdate("CASCADE");
+        table.primary("user_id", "post_id");
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("View");
+};
---
+++ models/seeds/04-views.js
@@ -0,0 +1,13 @@
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('View').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('View').insert([
+        {
+          user_id: 1,
+          post_id: 1
+        }
+      ]);
+    });
+};
