90473ab13784fbfac43061c8a4dd809bebf52bc8
Natividad Alvarez <tivialvarez@gmail.com>
2020-10-15 09:31:10 -0400

    Added more data to posts seed

--- models/seeds/03-posts.js
+++ models/seeds/03-posts.js
@@ -10,6 +10,27 @@ exports.seed = function(knex) {
           author_id: 1,
           title: "Welcome to the website",
           body: "Let's hope this turns out nice :)"
+        },
+        {
+          id: 2,
+          community_id: 1,
+          author_id: 1,
+          title: "How many times",
+          body: "How many? Will we know?"
+        },
+        {
+          id: 3,
+          community_id: 1,
+          author_id: 1,
+          title: "I like this place",
+          body: "blah blah blah blah, it's nice :)"
+        },
+        {
+          id: 4,
+          community_id: 1,
+          author_id: 1,
+          title: "Why is the sky blue",
+          body: "and why is this a dumb post"
         }
       ]);
     });
