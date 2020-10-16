dc824d2f0d25b35df473978c0df6440c3f226845
Natividad Alvarez <tivialvarez@gmail.com>
2020-10-16 02:35:48 -0400

    Updated primary key to be composite key on views table

--- models/migrations/20201015130021_create_views_table.js
+++ models/migrations/20201015130021_create_views_table.js
@@ -2,7 +2,7 @@ exports.up = function(knex) {
     return knex.schema.createTable("View", table=>{
         table.integer("user_id").notNullable().references("id").inTable("User").onDelete("CASCADE").onUpdate("CASCADE");
         table.integer("post_id").notNullable().references("id").inTable("Post").onDelete("CASCADE").onUpdate("CASCADE");
-        table.primary("user_id", "post_id");
+        table.primary(["user_id", "post_id"]);
     });
 };
 
