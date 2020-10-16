49672bb313259666d53f244a07b5fe0312a24a65
Natividad Alvarez <tivialvarez@gmail.com>
2020-10-16 07:00:28 -0400

    Wrapped postPreview animations in function

--- public/js/animations.js
+++ public/js/animations.js
@@ -1,10 +1,16 @@
-document.querySelectorAll(".post-preview").forEach(postPreview=>{
-    postPreview.onmouseenter = function(){
-        this.classList.add("hover");
-        this.classList.remove("out");
-    }
-    postPreview.onmouseleave = function(){
-        this.classList.add("out");
-        this.classList.remove("hover")
-    }
-})
+//using a function so I can call this after dynamically rendering elements with js
+//after an getting posts from the db asynchronously
+const postPreviewAnimations = ()=> {
+    document.querySelectorAll(".post-preview").forEach(postPreview=>{
+        postPreview.onmouseenter = function(){
+            this.classList.add("hover");
+            this.classList.remove("out");
+        }
+        postPreview.onmouseleave = function(){
+            this.classList.add("out");
+            this.classList.remove("hover")
+        }
+    })
+}
+
+postPreviewAnimations();
