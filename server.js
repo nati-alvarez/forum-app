4357a34463c7125a5b2d8846a2553f98023a80be
Natividad Alvarez <tivialvarez@gmail.com>
2020-10-21 03:10:20 -0400

    Setup helmet and express-session

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const server = express();
 const path = require("path");
+const helmet = require("helmet");
+const session = require("session");
 
 server.use(express.static(path.join(__dirname, "public")));
 server.set("view engine", "ejs");
@@ -9,6 +11,20 @@ server.set("view engine", "ejs");
 server.use(express.urlencoded({extended: true}));
 server.use(express.json());
 
+server.use(helmet());
+server.use(session({
+    name: "JASDOIS9SDFA9FA",
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        path: "/",
+        httpOnly: true,
+        secure: true,
+        sameSite: true,
+        magAge: 7 * 24 * 60 * 60 * 100
+    }
+}));
 
 const routes = require("./routes");
 server.use("/", routes)
