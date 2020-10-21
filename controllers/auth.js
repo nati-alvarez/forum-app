18ef7f99b47ef7ac38214b9f4b330866e408fba2
Natividad Alvarez <tivialvarez@gmail.com>
2020-10-21 04:05:27 -0400

    Created users controller and routes

---
+++ controllers/auth.js
@@ -0,0 +1,24 @@
+const UserModel = require("../models/users/index");
+const bcrypt = require("bcrypt");
+
+class AuthController {
+    static async login(req, res){
+        const {username, password} = req.body;
+        if(!username || !password) return res.status(400).json({err: "Username and password required"});
+
+        const user = await UserModel.findByUsername(username);
+        if(!user) return res.status(401).json({err: "Incorrect username or password"});
+        
+        const passwordsMatch = bcrypt.compareSync(password, user.password);
+        if(!passwordsMatch) return res.status(401).json({err: "Incorrect username or password"});
+
+        req.session.user = {
+            id: user.id,
+            username: user.username,
+            pfp: user.pfp
+        }
+        res.status(200).json({message: "Login successful"});
+    }
+}
+
+module.exports = AuthController;
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,10 @@ const homepageController = require("../controllers/homepage");
 
 router.get("/", homepageController.renderHomepage);
 
+const authRoutes = require("./auth");
 const postRoutes = require("./posts");
+
+router.use("/", authRoutes);
 router.use("/posts", postRoutes);
 
 module.exports = router;
