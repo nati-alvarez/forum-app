8e7f52f3cd11af0f1e0cb751ee508d8a0dffc30f
Natividad Alvarez <tivialvarez@gmail.com>
2020-10-22 17:13:52 -0400

    Made community controller

---
+++ controllers/community.js
@@ -0,0 +1,14 @@
+const communityModel = require("../models/community/index");
+
+class CommunityController {
+    static async renderCommunityPage(req, res){
+        try {
+            const community = await communityModel.getCommunity(req.params.community_name);
+            res.render("community-page", {user: req.session.user, community});
+        }catch(err){
+            console.log(err)
+        }
+    }
+}
+
+module.exports = CommunityController;
