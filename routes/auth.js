65f8295f640273c6ca09cbc1893e87f39fa9650c
Natividad Alvarez <tivialvarez@gmail.com>
2020-10-22 03:26:00 -0400

    Setup multer and cloudinary

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
+const multer = require("multer");
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './tmp/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+})
+const upload = multer({storage});
 const authController = require("../controllers/auth");
+
 
 router.get("/login",  authController.renderLoginPage);
 router.post("/login", authController.login);
+
+router.get("/signup",  authController.renderSignupPage)
+router.post("/signup", upload.single("pfp"), authController.signup);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const server = express();
 const path = require("path");
 const helmet = require("helmet");
 const session = require("express-session");
+const cloudinary = require("cloudinary");
 
 server.use(express.static(path.join(__dirname, "public")));
 server.set("view engine", "ejs");
@@ -25,6 +26,12 @@ server.use(session({
         expires: 7 * 24 * 60 * 60 * 100
     }
 }));
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_SECRET
+});
 
 const routes = require("./routes");
 server.use("/", routes);
