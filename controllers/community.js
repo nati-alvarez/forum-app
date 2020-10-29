1caf4b84aad0d6ca994790f7e92f9d9350e5a41e
Natividad Alvarez <tivialvarez@gmail.com>
2020-10-29 08:22:58 -0400

    setting isMember property on community that tells if a user is a part of a community

--- controllers/community.js
+++ controllers/community.js
@@ -4,6 +4,10 @@ class CommunityController {
     static async renderCommunityPage(req, res){
         try {
             const community = await communityModel.getCommunity(req.params.community_name);
+            if(req.user) {
+                const isMember = await communityModel.isMember(req.user.id, req.params.community_name);
+                community.isMember = isMember;
+            }
             res.render("community-page", {user: req.user, community});
         }catch(err){
             console.log(err)
