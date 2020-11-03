37ae3d7f84195c7196734111323af3bc3766982c
Natividad Alvarez <tivialvarez@gmail.com>
2020-11-03 14:21:52 -0500

    Added jsdocs for cloudinary helper

--- helpers/cloudinary.js
+++ helpers/cloudinary.js
@@ -1,10 +1,18 @@
 const cloudinary = require("cloudinary").v2;
 
+/**
+ * Uploads the given file to cloudinary and returns promise
+ * @param {String} path the path of the image to upload
+ */
 function uploadFile(path){
     return new Promise((resovle, reject)=>{
-        cloudinary.uploader.upload(path, (err, url)=>{
+        cloudinary.uploader.upload(path, (err, image)=>{
             if (err) return reject(err);
-            return resovle(url);
+            return resovle(image.secure_url);
         });
     })
 }
+
+module.exports = {
+    uploadFile
+};
