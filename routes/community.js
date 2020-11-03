45357dceb2e9205357f869e98f02b79d557c2961
Natividad Alvarez <tivialvarez@gmail.com>
2020-11-03 13:03:24 -0500

    Created endpoint for making a new community. New endpoint uses multer for file uploads

--- routes/community.js
+++ routes/community.js
@@ -2,9 +2,20 @@ const express = require("express");
 const router = express.Router();
 const communityController = require("../controllers/community.js");
 const authMiddleware = require("../middleware/auth");
+const multer = require("multer");
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './tmp/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+})
+const upload = multer({storage});
 
 
 router.get("/create", authMiddleware.protected, communityController.renderCreateCommunityPage);
+router.post("/create", [authMiddleware.protected, upload.fields([{name: "banner"}, {name: "icon"}])], communityController.createCommunity);
 router.get("/:community_name", communityController.renderCommunityPage);
 
 module.exports = router;
