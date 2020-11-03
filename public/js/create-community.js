492760d4c3c8551401b37f81f7ff481379416166
Natividad Alvarez <tivialvarez@gmail.com>
2020-11-03 14:30:13 -0500

    createCommunity function makes post request to /community/create endpoint

--- public/js/create-community.js
+++ public/js/create-community.js
@@ -6,20 +6,29 @@ const descriptionInput = document.querySelector("textarea#description");
 const bannerInput = document.querySelector("input#banner");
 const iconInput = document.querySelector("input#icon");
 
-function createCommunity(e){
+async function createCommunity(e){
     e.preventDefault();
     requestStatus.renderLoading();
     const formData = new FormData();
     
-    if(!nameInput.value.trim()) return requestStatus.renderError("Name is required");
-    formData.append("name", nameInput.value.trim());
+    const name = nameInput.value.trim()
+    if(!name) return requestStatus.renderError("Name is required");
+    formData.append("name", name);
 
-    if(!descriptionInput.value.trim()) return requestStatus.renderError("Description is required");
-    formData.append("description", nameInput.value.trim());
+    const description = descriptionInput.value.trim();
+    if(!description) return requestStatus.renderError("Description is required");
+    formData.append("description", description);
 
     const bannerImage = bannerInput.files[0] || null;
     const iconImage = iconInput.files[0] || null;
     formData.append("banner", bannerImage);
     formData.append("icon", iconImage);
 
+    const res = await fetch("/community/create", {
+        method: "POST",
+        body: formData
+    });
+    data = await res.json();
+    if(res.status !== 201) requestStatus.renderError(data.err);
+    else if(res.status === 201) window.location = `/community/${name}`
 }  
